dc056e87db2d5866aec141646433c8357f052f79
ccasner <charlottecasner@gmail.com>
2017-12-05 20:23:00 -0500

    Adds api keys to config/secrets.yml
    - deleted unused file
    - renamed file for stashing curl script notes

---
+++ scripts/weather/get-weather.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+
+
+# API="${API_ORIGIN:-https://api.darksky.net/forecast/}"
+# URL_PATH="/${DS_SECRET_KEY}/${LAT},${LONG}"
+# curl "${API}${URL_PATH}" \
+#   --include \
+#   --request GET \
+#   --
+#   # --header "Authorization: Token token=$TOKEN"
+
+echo
