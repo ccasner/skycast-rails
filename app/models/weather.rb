768145c9f68279d67cce99f5cab8d2f4fa1c39da
ccasner <charlottecasner@gmail.com>
2017-12-06 17:36:22 -0500

    Sets api key for darkSky in model
    - uses darksky gem and instantiates new api with secret key from .env
    - sets the forecast to variable which will eventually be sent back to
    client

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,2 +1,6 @@
 class Weather < ApplicationRecord
+  require 'darksky'
+  LOCATION = %w[:longitude :latitude]
+  darksky = Darksky::API.new(ENV['DS_SECRET_KEY'])
+  forecast = darksky.forecast(*LOCATION)
 end
