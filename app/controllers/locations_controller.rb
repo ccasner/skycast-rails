a7aaaaadcc50d708072f6f4779ba447f9f45dde0
ccasner <charlottecasner@gmail.com>
2017-12-06 12:37:43 -0500

    Edits parameter to correct name
    - in location_params function changed require(:location) to :address

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -46,6 +46,6 @@ class LocationsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def location_params
-      params.require(:location).permit(:address, :user)
+      params.require(:address).permit(:address, :user)
     end
 end
