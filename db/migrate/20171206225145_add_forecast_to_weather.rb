dd70d1d1d0914200afccb8edb406c3ebea19ab3b
ccasner <charlottecasner@gmail.com>
2017-12-06 17:53:12 -0500

    Runs migration to add forecast column to weather
    - sets forecast datatype json to column in weather resource
    - adds forecast to weather serializer

---
+++ db/migrate/20171206225145_add_forecast_to_weather.rb
@@ -0,0 +1,5 @@
+class AddForecastToWeather < ActiveRecord::Migration[5.1]
+  def change
+    add_column :weathers, :forecast, :json
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206220741) do
+ActiveRecord::Schema.define(version: 20171206225145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171206220741) do
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "forecast"
   end
 
   add_foreign_key "examples", "users"
