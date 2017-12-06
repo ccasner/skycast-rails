e29f21449dc1b5d58d6bccf5f30389219e676b84
ccasner <charlottecasner@gmail.com>
2017-12-06 17:25:53 -0500

    Runs migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206040139) do
+ActiveRecord::Schema.define(version: 20171206220741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20171206040139) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
+  end
+
+  create_table "weathers", force: :cascade do |t|
+    t.float "longitude"
+    t.float "latitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "examples", "users"
